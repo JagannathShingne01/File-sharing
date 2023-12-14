668b995b5291aa20a580306d2bd59c8e28c4da74
JagannathShingne01 <jagannathshingne01@gmail.com>
2023-12-14 22:08:43 +0530

    sign-up added using clerk

---
+++ app/(dashboard)/(routes)/files/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+const files = () => {
+  return (
+    <div>file
+        <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
+}
+
+export default files
