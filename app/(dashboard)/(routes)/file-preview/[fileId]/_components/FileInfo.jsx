23642e90bd26d70337d9fa373e44dbb7559e6119
JagannathShingne01 <jagannathshingne01@gmail.com>
2023-12-21 22:15:22 +0530

    file preview added

--- app/(dashboard)/(routes)/file-preview/[fileId]/_components/FileInfo.jsx
+++ app/(dashboard)/(routes)/file-preview/[fileId]/_components/FileInfo.jsx
@@ -14,7 +14,7 @@ const FileInfo = ({file}) => {
   return file&&(
     <>
     <div className='text-center border border-blue-200 flex justify-center m-4 flex-col items-center p-2 rounded-md '>
-      <Image src={fileType=='image' ? file?.fileUrl :'/file.png'} alt='file' width={100} height={100} className='h-[200px] rounded-md object-contain'/>                            
+      <Image src={fileType==['image','png'] ? file?.fileUrl :'/file.png'} alt='file' width={100} height={100} className='h-[200px] rounded-md object-contain'/>                            
       <div>
         <h2>{file.fileName}</h2>
         <h2 className='text-gray-400 text-[13px]'>{file.fileType}</h2>
